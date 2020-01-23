c52688ffbd43161a3ac5dc083f7233387314dd48
JeevaNantham <47416826+JeevaNanthamWEBDEVELOPMENT@users.noreply.github.com>
2020-01-23 08:47:24 -0800

    updated network monitoring tool
    To resolved the connection establishment bugs and some exceptional errors

--- network monitoring tool/flask_app.py
+++ network monitoring tool/flask_app.py
@@ -4,23 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def call_index():
-   return render_template("index1.html")
+   return render_template("index1.html",name = "connection not established")
 
-@app.route('/', methods= ['POST'])
+@app.route('/index', methods= ['POST'])
 def index_netstat():
     net_address1 = request.form['net_address']
     sec = int(request.form['secs'])
     sec_iter = sec
+    ##################################################run ping with given user net address
     web_ping = os.popen("ping " + net_address1).read()
     ping = "".join(re.sub(r"[\n\t]*", "", web_ping))
     concat = ""
     plotfile = "plots.csv"
-    ######clear csv
+    ####################################################################clear csv
     imp = open(plotfile, 'r')
     out = open(plotfile, 'w')
     writer = csv.writer(out)
     for row in csv.reader(imp):
         writer.writerow(row)
+    ##################################################finding average and connection established or not
     for i in range(len(ping) - 1, 0, -1):
         try:
             if ping[i] == "," or ping[i] == ".":
@@ -36,23 +38,25 @@ def index_netstat():
             pass
     concat = concat[::-1]
     plotfile = "plots.csv"
-    ######clear csv
+    ##################################################clear csv
     imp = open(plotfile, 'r')
     out = open(plotfile, 'w')
     writer = csv.writer(out)
     for row in csv.reader(imp):
         writer.writerow(row)
-    ########ping result
+    #####################################################save ping result in csv
     with open(plotfile, 'a',newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(concat[::-1])
-    ######netstat for chart
+    #######################################################real time chart
     plotx_list = []
     ploty_list = []
     iteration = 0
+    #######################################################real time chart generation
     while (iteration<30):
         iteration +=1
         con = ""
+        #######################################################getting data flow rate
         for j in range(sec - sec_iter + 1, sec + 1):
             msg = os.popen("netstat -e").read()
             netstat = "".join(re.sub(r"[\n\t]*", "", msg))
@@ -70,6 +74,7 @@ def index_netstat():
             con = ""
         con = ""
         n3_sec = (sec + sec_iter)
+        #######################################################getting data flow rate
         for j in range(sec + 1, sec + sec_iter + 1):
             msg = os.popen("netstat -e").read()
             netstat = "".join(re.sub(r"[\n\t]*", "", msg))
@@ -85,13 +90,14 @@ def index_netstat():
                 n2_sec = int(con)
                 break
             con = ""
+        #######################################################calculating difference
         plot = abs(n1_sec - n2_sec) / 1024
         plotx_list.append(sec)
         ploty_list.append(plot)
         rows = [[sec,plot]]
         sec += sec_iter
         plotfile = "plots.csv"
-        #######append plot to csv
+        ######################################################append difference between the netstat of the plot to csv
         with open(plotfile, 'a',newline="") as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerows(rows)
@@ -101,6 +107,7 @@ def index_netstat():
 def saved_data():
     plotfile = "plots.csv"
     con = ""
+    #######################################################ploting saved real time chart
     with open(plotfile, 'r') as f:
         reader = csv.reader(f)
         your_list = list(reader)
